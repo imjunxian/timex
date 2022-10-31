5a444ab0c3ca71ac4c98799ae39c87401b8ece76
CallMeJaX <87804946+C411M3J4X@users.noreply.github.com>
2022-10-31 15:13:18 +0800

    commit config file

--- dbconfig.php
+++ dbconfig.php
@@ -8,6 +8,7 @@ session_start();
 $projectID = 'timex-338211';
 $keyFilePath = 'C:\xampp\htdocs\timex\timex-338211-firebase-adminsdk-frpng-8d427011d4.json';
 
+Stripe::setApiKey('sk_test_51KFH6RCFJt3ZkTxSJLa9c20sVaxygf3CTqRqn0CHiJNB2rvpXdgQ2cjjlssfL8SAYu2aixWenp2XuhBRksiux2KA00NcSlkwkT');
 $stripe = new \Stripe\StripeClient('sk_test_51KFH6RCFJt3ZkTxSJLa9c20sVaxygf3CTqRqn0CHiJNB2rvpXdgQ2cjjlssfL8SAYu2aixWenp2XuhBRksiux2KA00NcSlkwkT');
 
 $db = new FirestoreClient([
